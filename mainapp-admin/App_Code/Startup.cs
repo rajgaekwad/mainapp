b572ed5beb85c9c1ae26d6ce4b2aa3b722177c3a
rajgaekwad <rajgaekwad@gmail.com>
2019-05-15 17:26:10 +0530

    all files

---
+++ mainapp-admin/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(mainapp_admin.Startup))]
+namespace mainapp_admin
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
---
+++ mainapp-adminsite/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(mainapp_adminsite.Startup))]
+namespace mainapp_adminsite
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
